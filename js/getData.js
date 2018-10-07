1671d3ec96088bf1da359994aacd543424152b79
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2018-10-07 15:28:45 -0600

    feat: got sample data

---
+++ js/getData.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const sampleData = async (start = 1, limit = 100, sort = 'rank') => {
+  try {
+    const cryptosByMarketCap = await axios.get(
+      `https://api.coinmarketcap.com/v2/ticker/?start=${start}&limit=${limit}&sort=${sort}`
+    );
+    return cryptosByMarketCap.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => console.log(await sampleData()))();
